6d30addd393c97135c1a422c1c0b4ff7c2e772f8
hamdan-27 <hamdan102703@gmail.com>
2023-05-01 02:40:25 +0400

    changed php include statement

--- drugsearchprocess.php
+++ drugsearchprocess.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<?php include('search.php') ?>
+<?php include('search_scripts.php') ?>
 <html>
 
 <head>
@@ -44,9 +44,6 @@
                 </div>
         </div>
     </div>
-
-
-
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
