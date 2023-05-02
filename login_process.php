3a2df62a97764c54678545f95b7cc4207dfa4baf
hamdan-27 <hamdan102703@gmail.com>
2023-05-02 16:01:41 +0400

    admin login done

--- login_process.php
+++ login_process.php
@@ -1,4 +1,5 @@
 <?php
+session_destroy();
 session_start();
 require('connection.php');
 
@@ -68,4 +69,26 @@ else if (isset($_POST['pharmalogin'])) {
         echo "<script> alert('Please enter your correct details'); </script>";
         echo "<script> open('pharma_login.php', '_self'); </script>";
     }
-}
+} // END PHARMACIST LOGIN PROCESS
+
+// ADMIN LOGIN PROCESS START
+if (isset($_POST['adminlogin'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM `admin` WHERE `username` = '$username' AND `password` = '$password'";
+    $result = mysqli_query($conn, $sql);
+    $final = mysqli_num_rows($result);
+
+    if ($final > 0) {
+        while ($row = mysqli_fetch_array($result)) { //used to fetch data from database
+            $_SESSION['loggedin'] = TRUE;
+            $_SESSION['name'] = $row['username'];
+            // $_SESSION['email'] = $row['email'];
+            header("Location: admin_panel.php");
+        }
+    } else {
+        echo "<script> alert('Please enter your correct details'); </script>";
+        echo "<script> open('admin_login.php', '_self'); </script>";
+    }
+} // END ADMIN LOGIN PROCESS
